0dc9caddb68052e6fc05ad2964f031095f1df68d
Aunsh Arekar <aunsharekar@gmail.com>
2025-01-24 15:48:58 -0600

    1- auth controllers added

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,20 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import cookieParser from 'cookie-parser'
 
 import authRoutes from './routes/auth.routes.js'
 import connectMongoDB from './db/connectMongoDB.js'
+
 
 dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT || 3000
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.use(cookieParser())
 
 app.use('/api/auth', authRoutes)
 
