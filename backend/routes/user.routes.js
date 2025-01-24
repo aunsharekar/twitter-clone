e0a0f0cea430dd664b39c97644d98647089f256b
Aunsh Arekar <aunsharekar@gmail.com>
2025-01-24 17:20:18 -0600

    1- user routes and controllers added

---
+++ backend/routes/user.routes.js
@@ -0,0 +1,12 @@
+import expres from 'express'
+import { protectRoute } from '../middleware/protectRoute.js';
+import { followUnfollowUser, getSuggestedUsers, getUserProfile, updateUser } from '../controllers/user.controller.js';
+
+const router = expres.Router()
+
+router.get("/profile/:username", protectRoute, getUserProfile)
+router.get("/suggested", protectRoute, getSuggestedUsers)
+router.post("/follow/:id", protectRoute, followUnfollowUser)
+router.post("/update", protectRoute, updateUser)
+
+export default router
